2312c596c7bdbab13242d0f53e3eb99bc466dbbc
SinghGursimar <singhgursimar@gmail.com>
2020-11-20 10:52:51 -0500

    2nd commit

--- src/test/java/steps/ProdLoginStep.java
+++ src/test/java/steps/ProdLoginStep.java
@@ -6,7 +6,7 @@ public class ProdLoginStep {
 	@And("user validates the capcha")
 	public void user_validates_the_capcha() {
 		System.out.println("@And -- user validates the capcha");
-	
+		System.out.println("OK OK OK");
 	}
 
 }
